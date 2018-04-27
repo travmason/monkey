002c6e7df74c71cd33a79f4b5132113535bd80c0
Trav Mason <travis.mason@gmail.com>
2018-04-27 16:35:20 +1000

    First socket.io working version.

--- app.js
+++ app.js
@@ -3,22 +3,39 @@ var express = require('express');
 var app = express();  
 var server = require('http').createServer(app); 
 var port =  process.env.port || 4200;
+var boolAnimationRunning = false;
 
 app.use(express.static(__dirname + '/node_modules'));  
 app.get('/', function(req, res,next) {  
     res.sendFile(__dirname + '/index.html');
 });
+app.get('/control', function(req, res,next) {  
+    res.sendFile(__dirname + '/control/controler.html');
+});
 	
-server = app.listen(process.env.port, function () {
+server = app.listen(port, function () {
   var addr = server.address();
   console.log('   app listening on http://' + addr.address + ':' + addr.port);
 });  
 var io = require('socket.io')(server);
 
 io.on('connection', function(client) {  
-	console.log('Client connected...');
 	client.on('join', function(data) {
-		console.log(data);
-		io.emit('messages', 'Hello from server');
+			console.log(data);
+			io.emit('message', 'Hello from the Server');
+		});
+	client.on('controller', function(data) {
+			switch (data) {
+				default:
+					console.log('Unknown command from controller');
+				case 'start':
+					console.log('Start from controller');
+					io.emit('command', 'start');
+					break;
+				case 'stop':
+					console.log('Stop from controller');
+					io.emit('command', 'stop');		
+					break;
+			}
 		});
     });
